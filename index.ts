2a58d0088f6d2766370593827b1f7c09e00984fa
Laf <luan.augusto.xavier.oliveira@gmail.com>
2021-03-06 01:20:19 -0300

    ⚙ SERVER_ADDRESS adicionado ao env

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 const app = require('./server');
 
-app.listen(process.env.SERVER_PORT, () => {
+app.listen(process.env.SERVER_PORT, process.env.SERVER_ADDRESS, () => {
     console.log(`[log]: Server iniciado em ${process.env.SERVER_PORT}`);
 });
